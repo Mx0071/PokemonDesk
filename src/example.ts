04854a5be993ebdd5bbc68d58fb09f32e64bc6ca
Max Onufrienko <maxim.onufrienko@gmail.com>
2020-10-29 23:09:03 +0200

    Made my 1st ts file.

---
+++ src/example.ts
@@ -0,0 +1,34 @@
+// 1.  concat('Hello ', 'World') // -> Hello World;
+function concat(word1: string, word2: string): string {
+	return `${word1}${word2}`;
+}
+const result = concat('Hello ', 'World') // -> Hello World;
+
+
+// 2. Interface
+type NumStringArray = Array<number|string>;
+
+interface Dictionary {
+    [Key: string]: string | NumStringArray;
+}
+
+interface HomeTaskInterface {
+		[Key: string]: string | NumStringArray | Array<Dictionary>;
+};
+
+const MyHometask: HomeTaskInterface = {
+	howIDoIt: "I Do It Wel",
+	simeArray: ["string one", "string two", 42],
+	withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+};
+
+
+// 3. Generics
+interface MyArray<T> {
+	[N: number]: T;
+
+	reduce(fn: (el1: T, el2: T) => T): T;
+};
+
+const tsArr: MyArray<number> = [1,2,3,10,4,5,6,8, 10.1];
+tsArr.reduce((a,b) => a + b);
