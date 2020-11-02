07ba1f1a4d57bf4987f83fc84b72f3ef2d2cd105
Max Onufrienko <maxim.onufrienko@gmail.com>
2020-11-03 00:30:00 +0200

    Added ESLint and Prettier

--- src/example.ts
+++ src/example.ts
@@ -1,34 +1,29 @@
-// 1.  concat('Hello ', 'World') // -> Hello World;
-function concat(word1: string, word2: string): string {
-	return `${word1}${word2}`;
-}
-const result = concat('Hello ', 'World') // -> Hello World;
+// // 1.  concat('Hello ', 'World') // -> Hello World;
+// function concat(word1: string, word2: string): string {
+//   return `${word1}${word2}`;
+// }
+// const result = concat('Hello ', 'World') // -> Hello World;
 
+// // 2. Interface
+// type NumStringArray = Array<number | string>;
 
-// 2. Interface
-type NumStringArray = Array<number|string>;
+// interface Dictionary {
+//   [Key: string]: string | NumStringArray;
+// }
 
-interface Dictionary {
-    [Key: string]: string | NumStringArray;
-}
+// interface HomeTaskInterface {
+//   [Key: string]: string | NumStringArray | Array<Dictionary>;
+// }
 
-interface HomeTaskInterface {
-		[Key: string]: string | NumStringArray | Array<Dictionary>;
-};
+// const MyHometask: HomeTaskInterface = {
+// 	howIDoIt: "I Do It Wel",
+// 	simeArray: ["string one", "string two", 42],
+// 	withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+// };
 
-const MyHometask: HomeTaskInterface = {
-	howIDoIt: "I Do It Wel",
-	simeArray: ["string one", "string two", 42],
-	withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
-};
+// // 3. Generics
+// interface MyArray<T> {
+//   [N: number]: T;
 
-
-// 3. Generics
-interface MyArray<T> {
-	[N: number]: T;
-
-	reduce<U>(fn: (el1: U, el2: T) => U, init: U): U;
-};
-
-const tsArr: MyArray<number> = [1,2,3,10,4,5,6,8, 10.1];
-console.log(tsArr.reduce((a, b) => a + b, 0));
+//   reduce<U>(fn: (el1: U, el2: T) => U, init: U): U;
+// }
