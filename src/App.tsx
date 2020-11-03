fbaa4e491e687f51c665b1abf71e1e01951e5331
Max Onufrienko <maxim.onufrienko@gmail.com>
2020-11-03 09:08:22 +0200

    Added Footer/Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import cn from 'classnames';
 import s from './App.module.scss';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
-  return <div className={cn(s.header, ' color')}>This is App Component!</div>;
+  return (
+    <div>
+      <Header />
+      <div className={cn(s.header, ' color')}>This is App Component!</div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
